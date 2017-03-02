77b9f42520e747e43183f42abf26f26ad92ba8ef
bsed <284178359@qq.com>
2017-03-02 09:43:15 +0800

    打开空调

---
+++ facade/src/main/java/facade/Screen.java
@@ -0,0 +1,17 @@
+package facade;
+
+/**
+ * 银幕
+ * 
+ * @author kelvin
+ *
+ */
+public class Screen {
+	public void up() {
+		System.out.println("升起银幕....");
+	}
+
+	public void down() {
+		System.out.println("下降银幕....");
+	}
+}
---
+++ facade/src/main/java/facade/Television.java
@@ -0,0 +1,17 @@
+package facade;
+
+/**
+ * 电视类
+ * 
+ * @author kelvin
+ *
+ */
+public class Television {
+	public void on() {
+		System.out.println("打开了电视....");
+	}
+
+	public void off() {
+		System.out.println("关闭了电视....");
+	}
+}
