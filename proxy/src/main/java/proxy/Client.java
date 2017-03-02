ad9e716a00c18e2cf4845d4731369b7add432a91
bsed <284178359@qq.com>
2017-03-02 11:03:24 +0800

    代理模式

---
+++ proxy/src/main/java/proxy/Client.java
@@ -0,0 +1,13 @@
+package proxy;
+
+public class Client {
+	public static void main(String[] args) {
+		BeautifulGirl mm = new BeautifulGirl("小屁孩...");
+
+		HerChum chum = new HerChum(mm);
+
+		chum.giveBook();
+		chum.giveChocolate();
+		chum.giveFlowers();
+	}
+}
